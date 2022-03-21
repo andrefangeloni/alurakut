2117205a71551965fc29b632c7265342b3605e93
André Angeloni <andrefangeloni@gmail.com>
2022-03-20 23:15:51 -0300

    feat: initial styled

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,37 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+
+  body {
+    font-family: sans-serif;
+    background: #d9e6f6;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
-}
+const App = ({ Component, pageProps }) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
+
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Box = styled.div`
+  background: #fff;
+  border-radius: 8px;
+`;
 
-export default function Home() {
-  return <Title>My page</Title>
-}
+const MainGrid = styled.main`
+  width: 100%;
+  grid-gap: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  padding: 16px;
+
+  .profileArea {
+    display: none;
+
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media (min-width: 860px) {
+    display: grid;
+    max-width: 1110px;
+    grid-template-areas: 'profileArea welcomeArea profileRelationsArea';
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
+
+const Home = () => (
+  <MainGrid>
+    <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+      <Box>Imagem</Box>
+    </div>
+
+    <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+      <Box>Bem vindo</Box>
+    </div>
+
+    <div
+      className="profileRelationsArea"
+      style={{ gridArea: 'profileRelationsArea' }}
+    >
+      <Box>Amigos</Box>
+      <Box>Comunidades</Box>
+    </div>
+  </MainGrid>
+);
+
+export default Home;
